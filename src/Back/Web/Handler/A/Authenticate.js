688f533ba9e3d424030ba4534fe878650b72152d
Alex Gusev <alex@flancer64.com>
2025-02-19 14:37:47 +0200

    SVTR-209 Add login page to the app

--- src/Back/Web/Handler/A/Authenticate.js
+++ src/Back/Web/Handler/A/Authenticate.js
@@ -3,35 +3,36 @@ import {constants as H2} from 'node:http2';
 // VARS
 const {
     HTTP2_HEADER_CONTENT_RESULT,
+    HTTP2_HEADER_LOCATION,
     HTTP2_METHOD_GET,
 } = H2;
 
 /**
- * Handler for processing ...
+ * Handler for processing OTP authentication when a user accesses the link from email
  */
 export default class Fl64_Auth_Otp_Back_Web_Handler_A_Authenticate {
     /**
-     * @param {Fl64_Auth_Otp_Back_Defaults} DEF
-     * @param {TeqFw_Core_Shared_Api_Logger} logger - Logger instance
-     * @param {TeqFw_Web_Back_App_Server_Respond} respond
-     * @param {TeqFw_Db_Back_App_TrxWrapper} trxWrapper
-     * @param {Fl64_Tmpl_Back_Service_Render} tmplRender
-     * @param {Fl64_Otp_Back_Mod_Token} modToken
-     * @param {Fl64_Web_Session_Back_Manager} mgrSession
-     * @param {Fl64_Auth_Otp_Back_Api_Adapter} adapter
-     * @param {typeof Fl64_Auth_Otp_Back_Enum_Token_Type} OTP_TYPE
-     * @param {typeof Fl64_Auth_Otp_Shared_Enum_Web_Result_Authenticate} RESULT
-     * @param {typeof Fl64_Tmpl_Back_Enum_Type} TMPL_TYPE
+     * @param {Fl64_Auth_Otp_Back_Defaults} DEF - Default configurations and constants
+     * @param {TeqFw_Core_Shared_Api_Logger} logger - Logger instance for logging actions and errors
+     * @param {TeqFw_Web_Back_Help_Respond} respond - Helper to respond to the client with HTTP codes and data
+     * @param {TeqFw_Db_Back_App_TrxWrapper} trxWrapper - Transaction wrapper for database operations
+     * @param {Fl64_Tmpl_Back_Service_Render} tmplRender - Template renderer to render HTML responses
+     * @param {Fl64_Otp_Back_Mod_Token} modToken - OTP token model to manage OTP tokens
+     * @param {Fl64_Web_Session_Back_Manager} session - Session manager for user session handling
+     * @param {Fl64_Auth_Otp_Back_Api_Adapter} adapter - Adapter to interact with external services for locales
+     * @param {typeof Fl64_Auth_Otp_Back_Enum_Token_Type} OTP_TYPE - Enum for OTP token types
+     * @param {typeof Fl64_Auth_Otp_Shared_Enum_Web_Result_Authenticate} RESULT - Enum for authentication results
+     * @param {typeof Fl64_Tmpl_Back_Enum_Type} TMPL_TYPE - Enum for template types
      */
     constructor(
         {
             Fl64_Auth_Otp_Back_Defaults$: DEF,
             TeqFw_Core_Shared_Api_Logger$$: logger,
-            TeqFw_Web_Back_App_Server_Respond$: respond,
+            TeqFw_Web_Back_Help_Respond$: respond,
             TeqFw_Db_Back_App_TrxWrapper$: trxWrapper,
             Fl64_Tmpl_Back_Service_Render$: tmplRender,
             Fl64_Otp_Back_Mod_Token$: modToken,
-            Fl64_Web_Session_Back_Manager$: mgrSession,
+            Fl64_Web_Session_Back_Manager$: session,
             Fl64_Auth_Otp_Back_Api_Adapter$: adapter,
             'Fl64_Auth_Otp_Back_Enum_Token_Type.default': OTP_TYPE,
             'Fl64_Auth_Otp_Shared_Enum_Web_Result_Authenticate.default': RESULT,
@@ -41,67 +42,90 @@ export default class Fl64_Auth_Otp_Back_Web_Handler_A_Authenticate {
         // VARS
         // MAIN
         /**
-         * Handles the provider selection action.
-         *
-         * @param {module:http.IncomingMessage|module:http2.Http2ServerRequest} req - Incoming HTTP request
+         * @param {module:http.IncomingMessage|module:http2.Http2ServerRequest} req - Incoming HTTP request object
          * @param {module:http.ServerResponse|module:http2.Http2ServerResponse} res - HTTP response object
          *
-         * @return {Promise<void>}
+         * @return {Promise<void>} - Returns a promise that resolves once the request is processed
          */
         this.run = async function (req, res) {
             // FUNCS
             /**
-             * @param {module:http.IncomingMessage|module:http2.Http2ServerRequest} req - Incoming HTTP request
+             * @param {module:http.IncomingMessage|module:http2.Http2ServerRequest} req - Incoming HTTP request object
              * @param {module:http.ServerResponse|module:http2.Http2ServerResponse} res - HTTP response object
-             * @returns {Promise<void>}
+             * @returns {Promise<void>} - Returns a promise that resolves once the GET request is processed
              */
             async function doGet(req, res) {
-                let result = RESULT.UNDEFINED;
-                // get token from URL
+                let result = RESULT.UNKNOWN_ERROR;
+
+                // Extract OTP token from the URL parameters
                 const url = new URL(req.url, `https://${req.headers.host}`);
                 const token = url.searchParams.get(DEF.SHARED.PARAM_TOKEN);
+
                 if (token) {
+                    // If a token is present, start a transaction to process it
                     await trxWrapper.execute(null, async (trx) => {
+                        // Read token details from the database
                         const {dto} = await modToken.read({trx, token});
                         if (dto && dto.type === OTP_TYPE.AUTHENTICATION) {
                             const userId = dto.user_ref;
-                            // initialize session
-                            await mgrSession.establish({
+
+                            // Initialize a session for the user if the token is valid
+                            await session.establish({
                                 trx,
                                 userId,
                                 httpRequest: req,
                                 httpResponse: res
                             });
+
+                            // Delete the token after successful authentication
                             await modToken.delete({trx, id: dto.id});
                             result = RESULT.SUCCESS;
                         } else {
-                            // token is not found or has a wrong type
+                            // Token not found or it has an invalid type
                             result = RESULT.WRONG_OTP;
                         }
                     });
                 } else {
                     result = RESULT.WRONG_OTP;
                 }
-                const {localeApp, localeUser} = await adapter.getLocales({req});
-                const {content} = await tmplRender.perform({
-                    pkg: DEF.NAME,
-                    type: TMPL_TYPE.WEB,
-                    name: 'authenticate.html',
-                    view: {
-                        isSuccess: result === RESULT.SUCCESS,
-                        isWrongOtp: result === RESULT.WRONG_OTP,
-                        isUnknownError: result === RESULT.UNKNOWN_ERROR,
-                    },
-                    localeUser,
-                    localeApp,
-                    localePkg: DEF.LOCALE,
-                });
-                respond.status200(res, content, {
-                    [HTTP2_HEADER_CONTENT_RESULT]: 'text/html'
-                });
+
+                // Redirect the user if a valid redirect URL is present in the session
+                const {url: redirectUrl} = await session.retrieveRedirectUrl({req, remove: true});
+                if ((result === RESULT.SUCCESS) && redirectUrl) {
+                    // Send a 303 redirect response
+                    respond.code303_SeeOther({
+                        res,
+                        headers: {[HTTP2_HEADER_LOCATION]: redirectUrl}
+                    });
+                    logger.info(`User is authenticated and redirected to '${redirectUrl}'.`);
+                } else {
+                    // Return a default web page with a message about login status
+                    const {localeApp, localeUser} = await adapter.getLocales({req});
+                    const {content: body} = await tmplRender.perform({
+                        pkg: DEF.NAME,
+                        type: TMPL_TYPE.WEB,
+                        name: 'authenticate.html',
+                        view: {
+                            isSuccess: result === RESULT.SUCCESS,
+                            isWrongOtp: result === RESULT.WRONG_OTP,
+                            isUnknownError: result === RESULT.UNKNOWN_ERROR,
+                        },
+                        localeUser,
+                        localeApp,
+                        localePkg: DEF.LOCALE,
+                    });
+
+                    // Respond with an HTML page and an appropriate message
+                    respond.code200_Ok({
+                        res,
+                        body,
+                        headers: {[HTTP2_HEADER_CONTENT_RESULT]: 'text/html'}
+                    });
+                }
             }
 
             // MAIN
+            // Handle only GET requests
             if (req.method === HTTP2_METHOD_GET) {
                 await doGet(req, res);
             }
--- src/Back/Web/Handler/A/Verify.js
+++ src/Back/Web/Handler/A/Verify.js
@@ -7,33 +7,34 @@ const {
 } = H2;
 
 /**
- * Handler for processing ...
+ * Handler for verifying the email address of a user during registration
+ * after they click the verification link sent to their email.
  */
 export default class Fl64_Auth_Otp_Back_Web_Handler_A_Verify {
     /**
-     * @param {Fl64_Auth_Otp_Back_Defaults} DEF
-     * @param {TeqFw_Core_Shared_Api_Logger} logger - Logger instance
-     * @param {TeqFw_Web_Back_App_Server_Respond} respond
-     * @param {TeqFw_Db_Back_App_TrxWrapper} trxWrapper
-     * @param {Fl64_Tmpl_Back_Service_Render} tmplRender
-     * @param {Fl64_Otp_Back_Mod_Token} modToken
-     * @param {Fl64_Web_Session_Back_Manager} mgrSession
-     * @param {Fl64_Auth_Otp_Back_Api_Adapter} adapter
-     * @param {Fl64_Auth_Otp_Back_Store_RDb_Repo_Email} repoEmail
-     * @param {typeof Fl64_Auth_Otp_Back_Enum_Token_Type} OTP_TYPE
-     * @param {typeof Fl64_Auth_Otp_Shared_Enum_Status} STATUS
-     * @param {typeof Fl64_Auth_Otp_Shared_Enum_Web_Result_Verify} RESULT
-     * @param {typeof Fl64_Tmpl_Back_Enum_Type} TMPL_TYPE
+     * @param {Fl64_Auth_Otp_Back_Defaults} DEF - Default configuration and constants
+     * @param {TeqFw_Core_Shared_Api_Logger} logger - Logger instance to log actions
+     * @param {TeqFw_Web_Back_Help_Respond} respond - Helper for sending HTTP responses
+     * @param {TeqFw_Db_Back_App_TrxWrapper} trxWrapper - Transaction wrapper for database interactions
+     * @param {Fl64_Tmpl_Back_Service_Render} tmplRender - Template renderer for generating HTML responses
+     * @param {Fl64_Otp_Back_Mod_Token} modToken - OTP token model for handling OTP tokens
+     * @param {Fl64_Web_Session_Back_Manager} session - Session manager to manage user sessions
+     * @param {Fl64_Auth_Otp_Back_Api_Adapter} adapter - API adapter for interacting with external services like locales
+     * @param {Fl64_Auth_Otp_Back_Store_RDb_Repo_Email} repoEmail - Repository for managing email-related data
+     * @param {typeof Fl64_Auth_Otp_Back_Enum_Token_Type} OTP_TYPE - Enum for OTP token types
+     * @param {typeof Fl64_Auth_Otp_Shared_Enum_Status} STATUS - Enum for email verification status
+     * @param {typeof Fl64_Auth_Otp_Shared_Enum_Web_Result_Verify} RESULT - Enum for the result of the verification process
+     * @param {typeof Fl64_Tmpl_Back_Enum_Type} TMPL_TYPE - Enum for template types
      */
     constructor(
         {
             Fl64_Auth_Otp_Back_Defaults$: DEF,
             TeqFw_Core_Shared_Api_Logger$$: logger,
-            TeqFw_Web_Back_App_Server_Respond$: respond,
+            TeqFw_Web_Back_Help_Respond$: respond,
             TeqFw_Db_Back_App_TrxWrapper$: trxWrapper,
             Fl64_Tmpl_Back_Service_Render$: tmplRender,
             Fl64_Otp_Back_Mod_Token$: modToken,
-            Fl64_Web_Session_Back_Manager$: mgrSession,
+            Fl64_Web_Session_Back_Manager$: session,
             Fl64_Auth_Otp_Back_Api_Adapter$: adapter,
             Fl64_Auth_Otp_Back_Store_RDb_Repo_Email$: repoEmail,
             'Fl64_Auth_Otp_Back_Enum_Token_Type.default': OTP_TYPE,
@@ -43,67 +44,83 @@ export default class Fl64_Auth_Otp_Back_Web_Handler_A_Verify {
         }
     ) {
         // VARS
+        // Retrieve email schema attributes from the email repository
         const A_EMAIL = repoEmail.getSchema().getAttributes();
 
         // MAIN
         /**
-         * Handles the provider selection action.
-         *
-         * @param {module:http.IncomingMessage|module:http2.Http2ServerRequest} req - Incoming HTTP request
+         * @param {module:http.IncomingMessage|module:http2.Http2ServerRequest} req - Incoming HTTP request object
          * @param {module:http.ServerResponse|module:http2.Http2ServerResponse} res - HTTP response object
          *
-         * @return {Promise<void>}
+         * @return {Promise<void>} - Resolves once the request has been processed
          */
         this.run = async function (req, res) {
             // FUNCS
             /**
-             * Generate HTTP form to register a email address.
+             * Process the GET request to verify the email using the OTP token.
              * @param {module:http.IncomingMessage|module:http2.Http2ServerRequest} req - Incoming HTTP request
              * @param {module:http.ServerResponse|module:http2.Http2ServerResponse} res - HTTP response object
-             * @returns {Promise<void>}
+             * @returns {Promise<void>} - Resolves once the email verification is done
              */
             async function doGet(req, res) {
-                let result = RESULT.UNDEFINED;
-                // get token from URL
+                let result = RESULT.UNKNOWN_ERROR;
+
+                // Retrieve token from the URL query parameters
                 const url = new URL(req.url, `https://${req.headers.host}`);
                 const token = url.searchParams.get(DEF.SHARED.PARAM_TOKEN);
+
                 if (token) {
+                    // If token exists, initiate a database transaction to process it
                     await trxWrapper.execute(null, async (trx) => {
+                        // Read the token data from the OTP token model
                         const {dto} = await modToken.read({trx, token});
                         if (dto && dto.type === OTP_TYPE.EMAIL_VERIFICATION) {
                             const userId = dto.user_ref;
+
+                            // Fetch the email record associated with the user ID
                             const {record: foundEmail} = await repoEmail.readOne({
                                 trx,
                                 key: {[A_EMAIL.USER_REF]: userId}
                             });
+
                             if (foundEmail && foundEmail.status === STATUS.UNVERIFIED) {
+                                // If the email is found and unverified, mark it as verified
                                 foundEmail.status = STATUS.VERIFIED;
                                 foundEmail.date_verified = new Date();
+
+                                // Update the email record in the database
                                 await repoEmail.updateOne({trx, updates: foundEmail});
                                 logger.info(`Email '${foundEmail.email}' is verified by user.`);
-                                // initialize session
-                                await mgrSession.establish({
+
+                                // Initialize a session for the user
+                                await session.establish({
                                     trx,
                                     userId,
                                     httpRequest: req,
                                     httpResponse: res
                                 });
+
+                                // Delete the OTP token after it has been used
                                 await modToken.delete({trx, id: dto.id});
                                 result = RESULT.SUCCESS;
                             } else {
-                                // token is not corresponded to valid email record
+                                // Token does not correspond to a valid email or the email is already verified
                                 result = RESULT.WRONG_OTP;
                             }
                         } else {
-                            // token is not found or has a wrong type
+                            // Token is either invalid or not of the correct type
                             result = RESULT.WRONG_OTP;
                         }
                     });
                 } else {
                     result = RESULT.WRONG_OTP;
                 }
+
+                // Retrieve the user's and app's locale settings for the response
                 const {localeApp, localeUser} = await adapter.getLocales({req});
-                const {content} = await tmplRender.perform({
+
+                // Render the verification result page with appropriate status
+                const {content: body} = await tmplRender.perform({
                     pkg: DEF.NAME,
                     type: TMPL_TYPE.WEB,
                     name: 'verify.html',
@@ -116,12 +133,17 @@ export default class Fl64_Auth_Otp_Back_Web_Handler_A_Verify {
                     localeApp,
                     localePkg: DEF.LOCALE,
                 });
-                respond.status200(res, content, {
-                    [HTTP2_HEADER_CONTENT_RESULT]: 'text/html'
+
+                // Send a response with the rendered content
+                respond.code200_Ok({
+                    res,
+                    body,
+                    headers: {[HTTP2_HEADER_CONTENT_RESULT]: 'text/html'}
                 });
             }
 
             // MAIN
+            // Process only GET requests for email verification
             if (req.method === HTTP2_METHOD_GET) {
                 await doGet(req, res);
             }
