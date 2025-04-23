8d58d4040330e4513ef5160712b84586374b596a
Alex Gusev <alex@flancer64.com>
2025-04-23 20:09:07 +0300

    WF-785 Add a set functionality to the Container

--- test/accept/common.mjs
+++ test/accept/common.mjs
@@ -4,7 +4,7 @@ import {join} from 'path';
 import 'dotenv/config';
 
 /**
- * @param {TeqFw_Di_Api_Container} container
+ * @param {TeqFw_Di_Container} container
  * @return {Promise<void>}
  */
 export async function dbConnect(container) {
@@ -15,7 +15,7 @@ export async function dbConnect(container) {
 }
 
 /**
- * @param {TeqFw_Di_Api_Container} container
+ * @param {TeqFw_Di_Container} container
  * @return {Promise<void>}
  */
 export async function dbDisconnect(container) {
@@ -29,7 +29,7 @@ export async function dbDisconnect(container) {
 }
 
 /**
- * @param {TeqFw_Di_Api_Container} container
+ * @param {TeqFw_Di_Container} container
  * @return {Promise<{user: {id: undefined}}>}
  */
 export async function dbCreateFkEntities(container) {
@@ -54,7 +54,7 @@ export async function dbCreateFkEntities(container) {
 }
 
 /**
- * @param {TeqFw_Di_Api_Container} container
+ * @param {TeqFw_Di_Container} container
  * @return {Promise<void>}
  */
 export async function dbReset(container) {
@@ -92,7 +92,7 @@ export async function dbReset(container) {
 }
 
 /**
- * @param {TeqFw_Di_Api_Container} container
+ * @param {TeqFw_Di_Container} container
  * @return {Promise<void>}
  */
 export async function initConfig(container) {
